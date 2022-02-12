3e5143aa14fd75242fff287581549f1ac85c9f31
Santhosh Maila <smaila@apple.com>
2022-02-12 13:23:07 -0800

    Adding the contactUs in the footer

--- src/app/shell/footer/footer.component.ts
+++ src/app/shell/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   currentDate: any;
-  constructor() {
+  constructor(private router: Router) {
     this.currentDate = new Date().getFullYear();
   }
 
   ngOnInit() {}
+
+  gotoContactus() {
+    this.router.navigate(['/contactUs'], { replaceUrl: true });
+  }
 }
